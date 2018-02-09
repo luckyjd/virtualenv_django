52ef5d5235cb0a0e7a02b995e1c55e52c5c5391b
luckyjd <narusunday@gmail.com>
2018-02-09 15:23:14 +0700

    install pgadmin4

---
+++ pgadmin4.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+cd /home/harry/Harry/Project/virtualenv_django/bin
+source activate
+python /home/harry/Harry/Project/virtualenv_django/lib/python3.5/site-packages/pgadmin4/pgAdmin4.py
+
