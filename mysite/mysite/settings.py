7a86770163c1e266da35ddfe1ee5da1a56469479
luckyjd <narusunday@gmail.com>
2018-02-09 09:43:29 +0700

    start mysite

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'cblwj2ys$6pp9*#t#9+^_f0nbi_r0d^q9+3$+2lp+t7_iw#9c+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django.mysite.com']
 
 
 # Application definition
@@ -118,3 +118,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
